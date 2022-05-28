36cfe2ccc22501e6d203c6a4171dd6b3125147e2
blomber666 <84716352+blomber666@users.noreply.github.com>
2022-05-28 02:32:20 +0200

    vns change

--- vns.py
+++ vns.py
@@ -20,43 +20,24 @@ def stochastic_2_opt(tour):
     return best_route
      
 
-def local_search(tour, neighbourhood_size = 5, attemps = 20):
-    
-    count = 0
-    solution = fun.deepcopy(tour)
-    
-    while (count < attemps): 
-        for i in range(0, neighbourhood_size):
-            candidate = stochastic_2_opt(solution)
-
-            if ( candidate[1] > solution[1] or (candidate[1] == solution[1] and candidate[2] < solution[2])):    #(candidate[1] / candidate[2]) > (best_solution[0][1] / best_solution[0][2])
-                solution = fun.deepcopy(candidate)
-                count = 0
-            else:
-                count += 1                             
-    
-    return solution  
-     
-    
-
-
 def variable_neighborhood_search(tour, neighbourhood_size = 5):
 
     solution = fun.deepcopy(tour)
-    best_solution = fun.deepcopy(tour)
+    count = 0
     
-    for i in range(0, neighbourhood_size):
-        for j in range(0, int(len(tour)/2)):
-            solution = stochastic_2_opt(best_solution) 
-        solution = local_search(solution, neighbourhood_size)
-        
-        if (solution[1] > best_solution[1] or (solution[1] == best_solution[1] and solution[2] < best_solution[2])):    #(candidate[1] / candidate[2]) > (best_solution[0][1] / best_solution[0][2])
-            best_solution = fun.deepcopy(solution)
-            break
+    while (count < neighbourhood_size):
+       candidate = stochastic_2_opt(solution)
 
-    print(f"\n\nSOLUZIONE stock-VNS --> {best_solution}\n\n") 
+       if ( candidate[1] > solution[1] or (candidate[1] == solution[1] and candidate[2] < solution[2])):    #(candidate[1] / candidate[2]) > (best_solution[0][1] / best_solution[0][2])
+           solution = fun.deepcopy(candidate)
+           count = 0
+       
+       else:
+           count += 1                             
+     
+    print(f"\n\nf\fSOLUZIONE stock-VNS --> {solution}\n\n") 
     
-    return best_solution
+    return solution
     
 
         
